b37a928068d55a4f68e3313db5e12531b0e87567
Alexandre Marquez de Moura Beicker <alexandremmbeicker@gmail.com>
2023-10-16 10:03:36 -0300

    criando componentes sociais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
-
 import { Home } from './pages/home'
 import { Admin } from './pages/admin'
 import { Login } from './pages/login'
 import { Networks } from './pages/networks'
+
 
 
 const router = createBrowserRouter([
