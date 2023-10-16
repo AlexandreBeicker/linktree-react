fcdfe90d532de366dda5fdbb5f966e2056e84c8a
Alexandre Marquez de Moura Beicker <alexandremmbeicker@gmail.com>
2023-10-16 14:14:48 -0300

    finalizando formulario

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from "../../components/Header"
 import { Input } from "../../components/Input"
 import { useState } from "react"
+import {FiTrash} from 'react-icons/fi'
 
 export function Admin(){
     const [nameInput, setNameInput] = useState("");
@@ -44,16 +45,37 @@ export function Admin(){
                         </div>
                     </section>
 
-                    <div className="flex items-center justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
+                    {nameInput !== '' && (
+                        <div className="flex items-center justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
                         <label className="text-white font-medium mt-2 mb-3">Veja como está ficando: </label>
                         <article 
                             className="w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
                             style={{marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput}}
                         >
-                            <p className="font-medium" style={{color: textColorInput}}>Canal do Youtube</p>
+                            <p className="font-medium" style={{color: textColorInput}}>{nameInput}</p>
                         </article>
-                    </div>
+                        </div>
+                    )}
+                    <button 
+                        type="submit"
+                        className="mb-7 bg-blue-600 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center">
+                        Cadastrar
+                    </button>
                 </form>
+                <h2 className="font-bold text-white mb-4 text-2xl">
+                    Meus links
+                </h2>
+                <article className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none"
+                style={{backgroundColor: "#2563EB", color: "#FFF"}}>
+                        <p>Canal do Youtube</p>
+                        <div>
+                            <button
+                                className="border border-dashed p-1 rounded bg-neutral-400"
+                            >
+                                <FiTrash size={18} color="#fff"/>
+                            </button>
+                        </div>
+                </article>
         </div>
     )
     }
